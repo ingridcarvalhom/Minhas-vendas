8125c35073257f44208a95701280fb64dc331064
a <as>
2019-08-20 13:44:25 -0300

    Adicionado ConsultaClientPorId.

--- tests/api/ClientsCest.php
+++ tests/api/ClientsCest.php
@@ -14,44 +14,46 @@ class ClientsCest
     public function _before(ApiTester $I){
 
         //Preciso de varions tokens aqui
-        $this->BearerToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjkwMTQ0YTM1ZjAwNjFlYmJmNDllZDJlYTIzNmUyNDc4ZDIxNWI4OGJhZmY4YmQ3NDAyZjA4MTVmNWMyMjk1M2I1ODI2YmE1MTIyYjgyMmVjIn0.eyJhdWQiOiIxIiwianRpIjoiOTAxNDRhMzVmMDA2MWViYmY0OWVkMmVhMjM2ZTI0NzhkMjE1Yjg4YmFmZjhiZDc0MDJmMDgxNWY1YzIyOTUzYjU4MjZiYTUxMjJiODIyZWMiLCJpYXQiOjE1NjQxNjk0ODIsIm5iZiI6MTU2NDE2OTQ4MiwiZXhwIjoxNTk1NzkxODgyLCJzdWIiOiJhNDJkMWIwYy01MjA1LTQyMWMtODRlZi01NWZiNjZiNjg0NGYiLCJzY29wZXMiOlsiY2FydC1yZWFkIiwiY2FydC13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiY29tcGFuaWVzLXdyaXRlIiwiY291cG9ucy1yZWFkIiwiY291cG9ucy13cml0ZSIsIm5vdGlmaWNhdGlvbnMtcmVhZCIsIm9yZGVycy1yZWFkIiwicHJvZHVjdHMtcmVhZCIsInByb2R1Y3RzLXdyaXRlIiwicHVyY2hhc2VzLXJlYWQiLCJzaGlwcGluZy1jYWxjdWxhdGUiLCJzaGlwcGluZy1jYW5jZWwiLCJzaGlwcGluZy1jaGVja291dCIsInNoaXBwaW5nLWNvbXBhbmllcyIsInNoaXBwaW5nLWdlbmVyYXRlIiwic2hpcHBpbmctcHJldmlldyIsInNoaXBwaW5nLXByaW50Iiwic2hpcHBpbmctc2hhcmUiLCJzaGlwcGluZy10cmFja2luZyIsImVjb21tZXJjZS1zaGlwcGluZyIsInRyYW5zYWN0aW9ucy1yZWFkIiwidXNlcnMtcmVhZCIsInVzZXJzLXdyaXRlIiwid2ViaG9va3MtcmVhZCIsIndlYmhvb2tzLXdyaXRlIiwibWluaGFzdmVuZGFzOjpjbGllbnRzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OmNsaWVudHM6d3JpdGUiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOndyaXRlIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczpyZWFkIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6c2hpcHBpbmdzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNoaXBwaW5nczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6Y29uY2lsaWF0aW9uczpyZWFkIiwibWluaGFzdmVuZGFzOjpjb25jaWxpYXRpb25zOndyaXRlIl19.3jovoMjzOqp7CgrJFKqkfA5BUzj5EBrBG0PNH6RmrUhA1H5sBuY4GC9NCRml21ScfRsKkRGDA2jHhbeuW4XBZ9qVE-2p0lPHw-ykygmU2csmBKNYBO70Utfqyd9PRRPC3xi_jm20CLBf8B7o1fsmCEEVWLBhQZz6SkKqbzbpAgsd0qEj5LWaIIS3dONu0KXo1pdU6liw27WYfrwZA7MIpILCkfSN9-4m40UotGVefU5dwiALYD_QUoAPfMZe9AvD0v9KorDQnsKrzJBvOIUy1zwEPV7tvLfpyRDhQU_OKslaahr45kX65ZMUZYOK3U50NA7nAr_wyyegNBGj3j2CoVwl-CXTWQSQkqjzltTF8YX3-kcr4vjP29fwN22QkxVVh_izREQJjF409MbiWVEBkYf-6N6DyL4h3vvOfJcZE2V4G0EVLAgUI7H1kKUkr37QKYhaJVkoLKrc8g_5nqedHQOt8LD5G5RSgiYnFfuosdLrFcE0FFgTToKfNHyfiNq61utwx2Pi7md5aY0XRRxzqdqPT4gqWpPSfG0_XyqE2htamWoXfwLIHBIyQufp8TAcpTolxpH8ANEyjE5jHNwb3sv0vpmhk5ks4AOGkftZlk7YYnupmPLDYhMWTp1Gr7Vq6AQ7JqrFJvMmMeE43mN00pPw1QH3sLEnHi5oVjweQ4s';
+        $this->BearerToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijk3YzliNDAwZTAxOWE0ZmJjOTkyYjJkMDBkNWE5ZGYwY2I1MDJkYWQ5OTlhNTNhMTZjMTVkNGJlMjg5ZTRhNmU3NGVkZmIxZDNkMjBmN2IyIn0.eyJhdWQiOiIxIiwianRpIjoiOTdjOWI0MDBlMDE5YTRmYmM5OTJiMmQwMGQ1YTlkZjBjYjUwMmRhZDk5OWE1M2ExNmMxNWQ0YmUyODllNGE2ZTc0ZWRmYjFkM2QyMGY3YjIiLCJpYXQiOjE1NjYzMTExODAsIm5iZiI6MTU2NjMxMTE4MCwiZXhwIjoxNTk3OTMzNTgwLCJzdWIiOiJmMDg4MDExZi1lNzZjLTQzOWQtYjYzNC01OWQ1MGNlYjBiNWUiLCJzY29wZXMiOlsiY2FydC1yZWFkIiwiY2FydC13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiY29tcGFuaWVzLXdyaXRlIiwiY291cG9ucy1yZWFkIiwiY291cG9ucy13cml0ZSIsIm5vdGlmaWNhdGlvbnMtcmVhZCIsIm9yZGVycy1yZWFkIiwicHJvZHVjdHMtcmVhZCIsInByb2R1Y3RzLXdyaXRlIiwicHVyY2hhc2VzLXJlYWQiLCJzaGlwcGluZy1jYWxjdWxhdGUiLCJzaGlwcGluZy1jYW5jZWwiLCJzaGlwcGluZy1jaGVja291dCIsInNoaXBwaW5nLWNvbXBhbmllcyIsInNoaXBwaW5nLWdlbmVyYXRlIiwic2hpcHBpbmctcHJldmlldyIsInNoaXBwaW5nLXByaW50Iiwic2hpcHBpbmctc2hhcmUiLCJzaGlwcGluZy10cmFja2luZyIsImVjb21tZXJjZS1zaGlwcGluZyIsInRyYW5zYWN0aW9ucy1yZWFkIiwidXNlcnMtcmVhZCIsInVzZXJzLXdyaXRlIiwid2ViaG9va3MtcmVhZCIsIndlYmhvb2tzLXdyaXRlIiwibWluaGFzdmVuZGFzOjpjbGllbnRzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OmNsaWVudHM6d3JpdGUiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOndyaXRlIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczpyZWFkIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6c2hpcHBpbmdzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNoaXBwaW5nczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6Y29uY2lsaWF0aW9uczpyZWFkIiwibWluaGFzdmVuZGFzOjpjb25jaWxpYXRpb25zOndyaXRlIl19.yphVmaWmIyIGgwItdsnpqx5xaQh7_LuR2GtC1BuW2G0XsieLSRp6oyA7FEKcU-oUFtagQgeDjRE_ozQfh9L8pvSXTGAwspWgTJW3CQyuxUveHYeF3zKtiXES7ZJerRTgBNob0VsRouz3t90Ve6kbSnPOndf3hhFKZ1JNdJFrehzoypdSjQ4WGDLa7SKnXOGqIvpqTirHDyaEwN7zqWOA_yR6XuzBRFG7inn52G_voxsTgFVWNsT1I0N0DHLwP1qyVrsVbKE-7tF1uj-QAx8g2JBxMVidlOagbJiq_FR41lACZOf4ryHayZwWIoRomlKkOp0tbsk65CB5vpIq2e9GHPdwsKbgXCYRdUqLckkosy6qqEkKfwWgKw-CcRpX3cpWLe27w2-zw1alwlUibEkXSKimsloFLmK9lf_muyjXNh0p9pBzEuifEFP5BniRjPOkS-nKS7MwMoH5zYNDkwEQ1cHCAXUz0MctjNMkYqPb8o8zIiNijRHDBuOkFfmlEdJGFVSAXO9j1OpZbRGznwVQcMUAx1Mhg3h0_kcy5yNMgsr9O5XdgoczilmCSPd5d_Tt7pCGutdW-HADWILGo3dJuhimz3Yltl1oB0oZvlP8LFimY1yIpIZ4UBFACEj2p5G8D8c00GAFM7YPEL1hZCHsYUqBE_BDnrvVK_gn4F9xf5U';
+      
         $this->clients_request = [
-            'own_id' => '1',
-            'account_id' => '7999e5fe-a6bf-4757-9feb-9ad513e2a0ea',
-            'name' => "Nome da Loja",
-            'url' => "https://develop.melhorenvio.com.br",
-            'settings'=> [
-                'address_id' => 33405,
-                'receipt' => false,
-                'own_hand' => false,
-                'services' => [1,2,3,4],
-                'dimensions' => [
-                    'width' => 12,
-                    'height' => 4,
-                    'length' => 17
-                ],
-                'jadlog_agency' => 0,
-                'default_service' => 'economic',
-                'insurance_value' => 'all'
-            ]
+            'own_id' => '999128',
+            'name' => "Shopping Online",
+            'url' => "https://www.loja.com",
+            'email' => 'email@daloja.com',
+            'document' => '87620890000112',
+            'address' => [
+                'postal_code'=> '96020150',
+                'street' => 'Rua X',
+                'number' => '123',
+                'complement' => 'Sala X',
+                'district' => 'Bairro',
+                'city' => 'Pelotas',
+                'state' => 'RS'
+            ],
         ];
-        
     }
 
 
     /**
-    * @dataprovider CadastrarErrorProvider
+    * @dataprovider CadastrarClientComErroProvider
     */
-    public function CadastrarError(APITester $I, \Codeception\Example $example){
+    public function CadastrarClientComErro(APITester $I, \Codeception\Example $example){
+
+        $I->wantTo("Cadastra Clientes Faltando Campos");
 
         $this->cadastro_endpoint = '/clients';
 
         $I->haveHttpHeader('Content-Type', 'application/json');
         $I->haveHttpHeader('Accept', 'application/json');
         $I->amBearerAuthenticated($this->BearerToken);
-
-        $this->clients_request[$example['missing']] = null;
        
+        if(isset($example['field'])){
+            $this->clients_request[$example['missing']][$example['field']] = null;
+        }else{
+            $this->clients_request[$example['missing']] = null;
+        }   
+        
         $I->sendPOST($this->cadastro_endpoint,$this->clients_request);
         $I->seeResponseCodeIsClientError();
         $I->seeResponseContains($example['missing']);
@@ -61,20 +63,26 @@ class ClientsCest
         ]);
     }
 
-    protected function CadastrarErrorProvider(){
+    protected function CadastrarClientComErroProvider(){
         return [
             ['missing'=>'name'],
             ['missing'=>'own_id'],
             ['missing'=>'url'],
-            ['missing'=>'account_id'],
-            ['missing'=>'settings']
+            ['missing'=>'email'],
+            ['missing'=>'document'],
+            ['missing'=>'address','field'=>'postal_code'],
+            ['missing'=>'address','field'=>'street'],
+            ['missing'=>'address','field'=>'number'],
+            ['missing'=>'address','field'=>'district'],
+            ['missing'=>'address','field'=>'city'],
+            ['missing'=>'address','field'=>'state']
         ];
     }
 
-    // /**
-    // * @dataprovider CadastrarClientProvider
-    // */
+
     public function CadastrarClient(ApiTester $I){
+
+        $I->wantTo("Cadastrar Clientes");
 
         $this->cadastro_endpoint = '/clients';
 
@@ -83,49 +91,44 @@ class ClientsCest
         $I->amBearerAuthenticated($this->BearerToken);
 
         $this->clients_request['own_id'] = strval(rand(0,1000000));
-        // $this->clients_request['name'] = $example['name'];
-        // $this->clients_request['url'] = $example['url'];
+
         $I->sendPOST($this->cadastro_endpoint,$this->clients_request);
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseMatchesJsonType([
             "data" => [
-                'id' => 'integer',
                 "app_id" => 'string',
                 "own_id" => 'string',
                 "name" => 'string',
                 "url" => 'string',
-                'settings' => 'array',
+                'settings' => [
+                   'receipt' => 'boolean',
+                   'own_hand' => 'boolean',
+                   'default_service' => 'string',
+                   'insurance_value' => 'string',
+                   'services' => 'array'
+                ],
                 'account_id' => 'string',
                 'user_id' => 'string',
-               
+                'id' => 'integer',           
             ]
         ]);
         
         $response = json_decode($I->grabResponse(),true);
+
         $I->assertEquals($this->clients_request['own_id'],$response['data']['own_id']);
-        $I->assertEquals($this->clients_request['account_id'],$response['data']['account_id']);
         $I->assertEquals($this->clients_request['name'],$response['data']['name']);
         $I->assertEquals($this->clients_request['url'],$response['data']['url']);
-        $I->assertEquals($this->clients_request['settings'],$response['data']['settings']);
+  
         $this->past_response = $response;
-
-    
     }
-
-    // protected function CadastrarClientProvider(){
-    //     return [
-    //         //TODO: Instalar o faker generator
-    //         ['name'=>'nome da loja','url'=>'https://loja.com'],
-    //         ['name'=>'segundo nome da loja','url'=>'https://loja2.com.br']
-    //     ];
-    // }
-
 
 
     /**
     * @dataprovider AtualizarClientProvider
     */
     public function AtualizarClients(ApiTester $I, \Codeception\Example $example){
+
+        $I->wantTo("Atualizar Clientes");
 
         $this->atualizar_endpoint = '/clients/';
 
@@ -155,13 +158,9 @@ class ClientsCest
             ]
         ]);
 
-
         $I->seeResponseContainsJson(array('own_id'=>$example['own_id']));
         $I->seeResponseContainsJson(array('name'=>$example['name']));
         $I->seeResponseContainsJson(array('url'=>$example['url']));
-
-
-
     }
 
     protected function AtualizarClientProvider(){
@@ -172,11 +171,9 @@ class ClientsCest
     }
 
 
-
-    // /**
-    //  * @before CadastrarClient
-    // */
     public function ConsultaClients(ApiTester $I){
+
+        $I->wantTo("Consultar Clientes");
 
         $this->consultar_endpoint = '/clients';
 
@@ -204,13 +201,45 @@ class ClientsCest
                 'total'=>'integer'
             ]
         ]);
-
-        // $I->seeResponseContainsJson($this->past_response);
     }
 
-   
 
+    /**
+    * @before CadastrarClient
+    */
+    public function ConsultaClientPorId(ApiTester $I){
 
+        $I->wantTo(" Verifica Consulta Client por Id");
 
-   
+        $this->consultar_endpoint = '/clients';
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->haveHttpHeader('Accept', 'application/json');
+        $I->amBearerAuthenticated($this->BearerToken);
+
+        $endpoint = $this->consultar_endpoint.'/'.$this->past_response['data']['id'];
+
+        $I->sendGET($endpoint);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseMatchesJsonType([
+            "data" => [
+                "app_id" => 'string',
+                "own_id" => 'string',
+                "name" => 'string',
+                "url" => 'string',
+                'settings' => [
+                   'receipt' => 'boolean',
+                   'own_hand' => 'boolean',
+                   'default_service' => 'string',
+                   'insurance_value' => 'string',
+                   'services' => 'array'
+                ],
+                'account_id' => 'string',
+                'user_id' => 'string',
+                'id' => 'integer',           
+            ]
+        ]);
+
+        $I->seeResponseContainsJson(array('id'=>$this->past_response['data']['id']));
+    }
 }
