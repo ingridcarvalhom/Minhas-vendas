4dbffa4baab1d5ffec390e832a15bc825e51d33e
a <as>
2019-09-11 15:19:55 -0300

    Refatorado ClientsCest.

--- tests/api/ClientsCest.php
+++ tests/api/ClientsCest.php
@@ -1,6 +1,7 @@
 <?php 
 
 use Codeception\Util\Fixtures;
+use \Codeception\Util\HttpCode;
 
 class ClientsCest
 {
@@ -10,8 +11,8 @@ class ClientsCest
 
         //Todo dia deve ser atualizado os tokens, criar um token em /painel gerenciar->tokens no ME,  
 
-        $this->tokenFromUserA = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjllYThiYzViNTUxMzFhODY5NDE1OTk5ZTc0M2Q3MzA0ZGM5MDYwYzY4ZTU5ZTY2YzNkNjkwODIzNTQ5MGNlOGMxNjk1YTRkZDIxMzNkMGI2In0.eyJhdWQiOiIxIiwianRpIjoiOWVhOGJjNWI1NTEzMWE4Njk0MTU5OTllNzQzZDczMDRkYzkwNjBjNjhlNTllNjZjM2Q2OTA4MjM1NDkwY2U4YzE2OTVhNGRkMjEzM2QwYjYiLCJpYXQiOjE1Njc2OTI3ODMsIm5iZiI6MTU2NzY5Mjc4MywiZXhwIjoxNTk5MzE1MTgzLCJzdWIiOiJmMDg4MDExZi1lNzZjLTQzOWQtYjYzNC01OWQ1MGNlYjBiNWUiLCJzY29wZXMiOlsiY2FydC1yZWFkIiwiY2FydC13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiY29tcGFuaWVzLXdyaXRlIiwiY291cG9ucy1yZWFkIiwiY291cG9ucy13cml0ZSIsIm5vdGlmaWNhdGlvbnMtcmVhZCIsIm9yZGVycy1yZWFkIiwicHJvZHVjdHMtcmVhZCIsInByb2R1Y3RzLXdyaXRlIiwicHVyY2hhc2VzLXJlYWQiLCJzaGlwcGluZy1jYWxjdWxhdGUiLCJzaGlwcGluZy1jYW5jZWwiLCJzaGlwcGluZy1jaGVja291dCIsInNoaXBwaW5nLWNvbXBhbmllcyIsInNoaXBwaW5nLWdlbmVyYXRlIiwic2hpcHBpbmctcHJldmlldyIsInNoaXBwaW5nLXByaW50Iiwic2hpcHBpbmctc2hhcmUiLCJzaGlwcGluZy10cmFja2luZyIsImVjb21tZXJjZS1zaGlwcGluZyIsInRyYW5zYWN0aW9ucy1yZWFkIiwidXNlcnMtcmVhZCIsInVzZXJzLXdyaXRlIiwid2ViaG9va3MtcmVhZCIsIndlYmhvb2tzLXdyaXRlIiwibWluaGFzdmVuZGFzOjpjbGllbnRzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OmNsaWVudHM6d3JpdGUiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOndyaXRlIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczpyZWFkIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6c2hpcHBpbmdzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNoaXBwaW5nczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6Y29uY2lsaWF0aW9uczpyZWFkIiwibWluaGFzdmVuZGFzOjpjb25jaWxpYXRpb25zOndyaXRlIl19.YfsdDGswyxmNM3bUDoEA7B1zxYoi4oKXb_rKuLM1EUX2AwVeZSDpMw_YLtCccORgO4z4C2jZYoz2BUxFo-weilzGnU2jtWo2nKC_UpytBRBnSVOE1Y2KYiIE78FtZQ53IOnaxnajmNDa57B1xKPZ3mzNnQwr3cGXylGk7jlu_qNxzpPA5jO7vJIJeRgwXf04_Vs-WgaNLrmRbdX2ebp27xZMt2T-p8L9VIWETGwwp_btZ6ICwdBLysaWk4NLCPgFtNxENFRotHAELL4x1FSpF59E1E7y8qCkBHKswREl7g2IMpl8Z9c8Aru3YZIoYL-PukjRy539qBetMloOq98zBLO49Jc8r4ZfG65V3h3np4kgRzW-vjWVbUciN7sgEbijCw_622zgsf0p5FoTzc7rH8JUQHKwsTotThjEfPNWJmPXiGhwYcx_fHOx3MYh7G_nmGOjA6MU3OyEfmGsWotHSf4ipc-e0N-t_o_gSZttSRKPuX3S-_4OFyKSBbBwBFjjd1OySxQhhhCySvx2JOHfrKPmHfVSm2AYNQvp1OKa7AaMzT3_ofV9OhTpJaev0o1iQuA_ofEsFlzBPVECHDKn4AtmmgdvD7ZOhOuy8rvXWIgWCpQY0dyzGxG4-6ATNqLQsvhkwefOLgldqPGEOpSxwza3yxUU5CO10XB2DUEHwjE';
-        $this->tokenFromUserB = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQzYTJhYzQ4N2E5ZWFlYWNmZmFkNzkxZmU0ZjhmY2ViOGE4ZWFiOTEzODc0ZjMyYjZmYmM1MDQ0NThmZDBkZTk0MWYxZTIwMWFmMWY4YWVkIn0.eyJhdWQiOiIxIiwianRpIjoiZDNhMmFjNDg3YTllYWVhY2ZmYWQ3OTFmZTRmOGZjZWI4YThlYWI5MTM4NzRmMzJiNmZiYzUwNDQ1OGZkMGRlOTQxZjFlMjAxYWYxZjhhZWQiLCJpYXQiOjE1Njc3MDUxOTgsIm5iZiI6MTU2NzcwNTE5OCwiZXhwIjoxNTk5MzI3NTk4LCJzdWIiOiIyMDM5Zjk1My0wNjNjLTQ3MjctYTQ4ZC0xMjQzNTI4M2UyOGUiLCJzY29wZXMiOlsiY2FydC1yZWFkIiwiY2FydC13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiY29tcGFuaWVzLXdyaXRlIiwiY291cG9ucy1yZWFkIiwiY291cG9ucy13cml0ZSIsIm5vdGlmaWNhdGlvbnMtcmVhZCIsIm9yZGVycy1yZWFkIiwicHJvZHVjdHMtcmVhZCIsInByb2R1Y3RzLXdyaXRlIiwicHVyY2hhc2VzLXJlYWQiLCJzaGlwcGluZy1jYWxjdWxhdGUiLCJzaGlwcGluZy1jYW5jZWwiLCJzaGlwcGluZy1jaGVja291dCIsInNoaXBwaW5nLWNvbXBhbmllcyIsInNoaXBwaW5nLWdlbmVyYXRlIiwic2hpcHBpbmctcHJldmlldyIsInNoaXBwaW5nLXByaW50Iiwic2hpcHBpbmctc2hhcmUiLCJzaGlwcGluZy10cmFja2luZyIsImVjb21tZXJjZS1zaGlwcGluZyIsInRyYW5zYWN0aW9ucy1yZWFkIiwidXNlcnMtcmVhZCIsInVzZXJzLXdyaXRlIiwid2ViaG9va3MtcmVhZCIsIndlYmhvb2tzLXdyaXRlIiwibWluaGFzdmVuZGFzOjpjbGllbnRzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OmNsaWVudHM6d3JpdGUiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNhbGVzOndyaXRlIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczpyZWFkIiwibWluaGFzdmVuZGFzOjp3ZWJob29rczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6c2hpcHBpbmdzOnJlYWQiLCJtaW5oYXN2ZW5kYXM6OnNoaXBwaW5nczp3cml0ZSIsIm1pbmhhc3ZlbmRhczo6Y29uY2lsaWF0aW9uczpyZWFkIiwibWluaGFzdmVuZGFzOjpjb25jaWxpYXRpb25zOndyaXRlIl19.Uhi3uNyBvxx0QAmPsJLJ7NzUFtYasag8UuzwcKFnmcAVlulUWgqV5QCL2TbYHgTi9OcD49Gri8TE-RpvFS1pYnu-6j6gLd8qarnkm9mL58_LFZ7f5cMq_cOEJFQbEgwykTIMMd6z-4iebxFbJ4kM_v8qqKaZugSpb74au6g6IKot46KFEPOvfZ3PgUTC-jIDkyal-m8v_QorXpb1zUUYkEidt_8fwHTbSwW78W_b8brkJILipRqGdNYQe-yA1d7ERBRLh6Po2jtr0tA6VwZN2xXkG9AX957wpy50eO4qGYRCoEtUFoeGSyfVmrC1THy_PbspI8Rt77F_wcPLiIAJnac2h7Um-h1VOHPvD3YLAziMJkYtVJ34c8kziT6Tw2RZrscWq2WoKqIuyZlSkF8qhcslu3t3OXRIiwOvpAV_6--HVVEXUIF_yzOua9RGDiguuXCwuxxk9fhmltKUq7sQ0I2_ehp90RWiQs6gUIA_iQwFtfkp_7_eenrXVdE_8ArLD1EhqP8ZlVZ1GjjN8dbrObFHNpPsCrokivXXyUJN6VWkFrdt2AaD03dz_kH3gYDTgiXtfjU-YRGH8IXYHmCuFsuU7qAsO5samx9Q_ieRKJDIeDw7sc2pqTraqHj5bJgOtdDwesGw-EZ219-pwaz1EBtFxxm9fij8DOxwpgMvrgc';
+        $this->tokenFromUserA = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjhjNDNjNDZmOGZiOTk2NWNmMDQ4MDBkYWI2ZWVjZjZjOTA5ZWQzMzcxMzgzM2FlN2UyNTI3N2MxZjZiNDhjZmQwNTdmYTVlYjQ5ZDE5NDRlIn0.eyJhdWQiOiIxIiwianRpIjoiOGM0M2M0NmY4ZmI5OTY1Y2YwNDgwMGRhYjZlZWNmNmM5MDllZDMzNzEzODMzYWU3ZTI1Mjc3YzFmNmI0OGNmZDA1N2ZhNWViNDlkMTk0NGUiLCJpYXQiOjE1NjgyMTYzOTUsIm5iZiI6MTU2ODIxNjM5NSwiZXhwIjoxNTk5ODM4Nzk1LCJzdWIiOiJmMDg4MDExZi1lNzZjLTQzOWQtYjYzNC01OWQ1MGNlYjBiNWUiLCJzY29wZXMiOlsiY2FydC1yZWFkIiwiY2FydC13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiY29tcGFuaWVzLXdyaXRlIiwiY291cG9ucy1yZWFkIiwiY291cG9ucy13cml0ZSIsIm5vdGlmaWNhdGlvbnMtcmVhZCIsIm9yZGVycy1yZWFkIiwicHJvZHVjdHMtcmVhZCIsInByb2R1Y3RzLXdyaXRlIiwicHVyY2hhc2VzLXJlYWQiLCJzaGlwcGluZy1jYWxjdWxhdGUiLCJzaGlwcGluZy1jYW5jZWwiLCJzaGlwcGluZy1jaGVja291dCIsInNoaXBwaW5nLWNvbXBhbmllcyIsInNoaXBwaW5nLWdlbmVyYXRlIiwic2hpcHBpbmctcHJldmlldyIsInNoaXBwaW5nLXByaW50Iiwic2hpcHBpbmctc2hhcmUiLCJzaGlwcGluZy10cmFja2luZyIsImVjb21tZXJjZS1zaGlwcGluZyIsInRyYW5zYWN0aW9ucy1yZWFkIiwidXNlcnMtcmVhZCIsInVzZXJzLXdyaXRlIiwid2ViaG9va3MtcmVhZCIsIndlYmhvb2tzLXdyaXRlIl19.LwD_NpKhnSfnqkoUGrTcqjMawTZXU_7kzgI-mwVdOPxMgamBSEHwKb2g4fO9_3_i26QvGZr0ouzU3ck-xMMz_A76unAHpnuiDQqIevMOMuoRXJI1sRdcQ--ICmFe0beUYB8jKh5KDBbJ_T5uu5KauGiNthRTTCnhiUh4zHliO7vf89s-EyY82ZtxLgL8CuasVnY3LYRB2TTInaT6BHRv_yVPkfXVBSSzYguCSnwRZEnKg8QpQj9LGiVDsUKM0MoGRO5JwDKDvmvknrMgPlS3oBNLhMMoyfvZss9BJFBeBsOPA18IrP8zH8jvN-V3X-PBSICg58delCTPjhvJnfmiLhesKkOzIVsaGeQf1JxUTN_7c1xs_H_6vo7VCo1OmntsH0A6cZaiNPxUtDt-mcfEIcOiKNwHz5Bqjzb4X5F9vgTB4YXlR47lHWLSHKZNpO5kwRolnisQxdWNZCV5z_wG3QIzghIwrZOmgECGD7X54FUxkGMgE_5oU5HU__qKcpo2t7PrAZC0hyO5FZrwNG2QzB3kNC_L1MQ8jyfaW0L94XW3LuSHHngt05Pg_18fQBApYk2JmJrzgIVcQs4d2E6P_s9pfCAezcOPBzvlFP8aLiaSHlTyKPaCR4YXdZ2ZsrPZHd_8P6tP8tRKVv6-z4nRFOhOsyXTLxw1EJowiQCsIw0';
+        $this->tokenFromUserB = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjJhNzc1NTRhMDA1MzFmYjIwZjczNDYyZDc0ZGFiZTE3YjczZDlmZTEzNDkxMGViM2ZhMGY3YmFmNzEzZDY3NDRhYzJhMWQ4MDU3ZTA2NjhkIn0.eyJhdWQiOiIxIiwianRpIjoiMmE3NzU1NGEwMDUzMWZiMjBmNzM0NjJkNzRkYWJlMTdiNzNkOWZlMTM0OTEwZWIzZmEwZjdiYWY3MTNkNjc0NGFjMmExZDgwNTdlMDY2OGQiLCJpYXQiOjE1NjgyMTY0MzMsIm5iZiI6MTU2ODIxNjQzMywiZXhwIjoxNTk5ODM4ODMzLCJzdWIiOiIyMDM5Zjk1My0wNjNjLTQ3MjctYTQ4ZC0xMjQzNTI4M2UyOGUiLCJzY29wZXMiOlsiY2FydC1yZWFkIiwiY2FydC13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiY29tcGFuaWVzLXdyaXRlIiwiY291cG9ucy1yZWFkIiwiY291cG9ucy13cml0ZSIsIm5vdGlmaWNhdGlvbnMtcmVhZCIsIm9yZGVycy1yZWFkIiwicHJvZHVjdHMtcmVhZCIsInByb2R1Y3RzLXdyaXRlIiwicHVyY2hhc2VzLXJlYWQiLCJzaGlwcGluZy1jYWxjdWxhdGUiLCJzaGlwcGluZy1jYW5jZWwiLCJzaGlwcGluZy1jaGVja291dCIsInNoaXBwaW5nLWNvbXBhbmllcyIsInNoaXBwaW5nLWdlbmVyYXRlIiwic2hpcHBpbmctcHJldmlldyIsInNoaXBwaW5nLXByaW50Iiwic2hpcHBpbmctc2hhcmUiLCJzaGlwcGluZy10cmFja2luZyIsImVjb21tZXJjZS1zaGlwcGluZyIsInRyYW5zYWN0aW9ucy1yZWFkIiwidXNlcnMtcmVhZCIsInVzZXJzLXdyaXRlIiwid2ViaG9va3MtcmVhZCIsIndlYmhvb2tzLXdyaXRlIl19.yRImf798O3hSOKMqLIyUmjD2mzEg4xsd-qHC1Yt3JOIk-RuiUhIZp5GjC2YfMkaYGQUJHE-ypq72JHzQhROQjv0wdkTnx39I8qXaBh4PoO5WDbXvAgwVXLn_TNGYxXiHTyeeVBCBO09cB9G-uUS9eebkbObi8qZ2C2KqIbanuuyYZFPxLMpqYfIpCBoBkQwAaqIMQIV2KLn-fRfbtpEvOuhaVZvBbmDjU0JCdWPeENSxaMAkppMkp-pLaF7a20NWcny8lWuz4gY8dKQGZ-Vi-9VCplzvia_7QhzV2EAXMpHgfBMpprHLIEzynVsvH1qKTVvKY9vdO7FHo6bEcsyeSQ8ohLT9hxphzV5WxyEz-VH2y0PkpxdPA9aqV9qosm3ylTTxHKhddNid1mqBB-1RicrTfBj3SUhP6HdcT9OALQqaiJbEOXgce93N0o-PNMWBf_xE08NF9uk7j8Lh0DkuRtvv8_JEu9zEnAdNZWDk_c54ltfiD0DPSbDc7l8tzmC0MawHZNiQ0B570HhxyF2AmZfk1D4bIoGfNkiXeaSZL61nZtRmdtG_SSNjQnttye4c5bSwz5SFMp6454baGE8yGsEN8dOkQaLP3baKU9JeHjlZISLu2NM8S-4UNQFBEybh1xzo_Fdmv1BWy_QuJ5s0_v3SRJ98dqZgu8QL-PkdZHQ';
 
         $this->clientsEndpoint = '/clients';
         $this->createClientRequest = [
@@ -33,7 +34,6 @@ class ClientsCest
 
         $I->haveHttpHeader('Content-Type', 'application/json');
         $I->haveHttpHeader('Accept', 'application/json');
-       
         $I->amBearerAuthenticated($this->tokenFromUserA);
     }
 
@@ -45,7 +45,9 @@ class ClientsCest
         $this->createClientRequest['own_id'] = strval(rand(0,1000000));
 
         $I->sendPOST($this->clientsEndpoint,$this->createClientRequest);
-        $I->seeResponseCodeIsSuccessful();
+        $response = json_decode($I->grabResponse(),true);
+
+        $I->seeResponseCodeIs(HttpCode::CREATED);
         $I->seeResponseMatchesJsonType([
             "data" => [
                 "app_id" => 'string',
@@ -65,8 +67,6 @@ class ClientsCest
             ]
         ]);
         
-        $response = json_decode($I->grabResponse(),true);
-
         $I->assertEquals($this->createClientRequest['own_id'],$response['data']['own_id']);
         $I->assertEquals($this->createClientRequest['name'],$response['data']['name']);
         $I->assertEquals($this->createClientRequest['url'],$response['data']['url']);
@@ -88,7 +88,8 @@ class ClientsCest
         }   
         
         $I->sendPOST($this->clientsEndpoint,$this->createClientRequest);
-        $I->seeResponseCodeIs(422); //Unprocessable Entity
+
+        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY); //Unprocessable Entity
         $I->seeResponseContains($example['missing']);
         $I->seeResponseMatchesJsonType([
             "message" => 'string',
@@ -116,7 +117,6 @@ class ClientsCest
     /**
     * @dataprovider UpdateClientProvider
     * @before CreateClientContract
-    * @depends CreateClientContract
     */
     public function UpdateClient(ApiTester $I, \Codeception\Example $example){
 
@@ -127,7 +127,10 @@ class ClientsCest
         $this->createClientRequest['url']    = $example['url'];
 
         $I->sendPATCH($this->clientsEndpoint.'/'.Fixtures::get('Client')['data']['id'],$this->createClientRequest);
-        $I->seeResponseCodeIsSuccessful();
+
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseContainsJson( array('name' => $example['name']) );
+        $I->seeResponseContainsJson( array('url' => $example['url']) );
         $I->seeResponseMatchesJsonType([
             "data" => [
                 'id' => 'integer',
@@ -141,9 +144,6 @@ class ClientsCest
 
             ]
         ]);
-
-        $I->seeResponseContainsJson( array('name' => $example['name']) );
-        $I->seeResponseContainsJson( array('url' => $example['url']) );
     }
 
     protected function UpdateClientProvider(){
@@ -154,12 +154,13 @@ class ClientsCest
     }
 
 
-    public function CheckClients(ApiTester $I){
+    public function CheckClient(ApiTester $I){
 
         $I->wantTo("Consultar Clientes");
 
         $I->sendGET($this->clientsEndpoint);
-        $I->seeResponseCodeIsSuccessful();
+
+        $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseMatchesJsonType([
             "data" => 'array',
             'links' => [
@@ -183,7 +184,7 @@ class ClientsCest
 
     /** 
     * @before CreateClientContract
-    * @depends CreateClientContract
+
     */
     public function CheckClientById(ApiTester $I){
 
@@ -192,7 +193,9 @@ class ClientsCest
         $createdTestClient = Fixtures::get('Client');
 
         $I->sendGET($this->clientsEndpoint.'/'.$createdTestClient['data']['id']);
-        $I->seeResponseCodeIsSuccessful();
+
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseContainsJson(array('id'=>$createdTestClient['data']['id']));
         $I->seeResponseMatchesJsonType([
             "data" => [
                 'id' => 'integer',   
@@ -211,36 +214,46 @@ class ClientsCest
                 'user_id' => 'string',                       
             ]
         ]);
-
-        $I->seeResponseContainsJson(array('id'=>$createdTestClient['data']['id']));
     }
 
     /** 
     * @before CreateClientContract
-   
+    * @before changeUser
     */
     public function DontSeeClientFromAnotherUser(ApiTester $I){
 
         $I->wantTo(" Assegura que um usuário não vê clientes de outro usuário");
 
-        $createdTestClient = Fixtures::get('Client');
+        $this->UnauthorizedGetClients($I);
+        $this->UnauthorizedGetClientsById($I);
+        $this->UnauthorizedPatchClients($I);
+        $this->UnauthorizedPostClients($I);
+    }
+
+    protected function changeUser(ApiTester $I){
         $I->deleteHeader('Authorization');
         $I->amBearerAuthenticated($this->tokenFromUserB);
+    }
 
+    protected function UnauthorizedGetClients(ApiTester $I){
         $I->sendGET($this->clientsEndpoint);
-        $I->dontSeeResponseContainsJson(array('id'=>$createdTestClient['data']['id']));
+        $I->dontSeeResponseContainsJson(array('id'=>Fixtures::get('Client')['data']['id']));
+    }
 
-        $I->sendGET($this->clientsEndpoint.'/'.$createdTestClient['data']['id']);
-        $I->seeResponseCodeIsClientError();
+    protected function UnauthorizedGetClientsById(ApiTester $I){
+        $I->sendGET($this->clientsEndpoint.'/'.Fixtures::get('Client')['data']['id']);
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
 
-        $I->sendPATCH($this->clientsEndpoint.'/'.$createdTestClient['data']['id'],$this->createClientRequest);
-        $I->seeResponseCodeIsClientError();
+    protected function UnauthorizedPatchClients(ApiTester $I){
+        $I->sendPATCH($this->clientsEndpoint.'/'.Fixtures::get('Client')['data']['id'],$this->createClientRequest);
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
 
+    protected function UnauthorizedPostClients(ApiTester $I){
         $this->createClientRequest['url'] = 'https://falhouTeste.com.br';
         $I->sendPOST($this->clientsEndpoint,$this->createClientRequest);
-        $I->seeResponseCodeIsClientError();
-
-
+        $I->seeResponseCodeIs(HttpCode::PRECONDITION_FAILED);
     }
 
 }
